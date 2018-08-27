0852291d9ef38f29f12f74bf0acfd7864813d20c
Rawan Sarhan <rawansar96@gmail.com>
2018-08-26 20:56:34 -0700

    Function to delete child files based on a path

---
+++ Clear-Space.ps1
@@ -0,0 +1,17 @@
+﻿function Clear-Space ([Parameter(Mandatory)][string] $path) {
+    Write-Output 'Path is: ' $path
+    $filesToDeleteByParent = Get-Item -Path $path | 
+    Get-ChildItem -Directory | Get-ChildItem -Directory | Sort -Property CreationTime | Select -Property FullName, CreationTime, Parent |
+    Group -Property Parent | Where -Property Count -gt 1 
+
+
+    foreach($files in $filesToDeleteByParent)
+    {
+        $oldVersionsToDelete = $files.Group | Select -ExpandProperty FullName -SkipLast 1 
+        Write-Output 'Deleting files at path: ' $oldVersionsToDelete
+        Remove-Item $oldVersionsToDelete -recurse 
+    }
+}
+
+
+
